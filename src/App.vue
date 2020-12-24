9562cabedb756b17f1075778387b541524522c03
QuaternionMark <markjarjour@gmail.com>
2020-12-24 15:28:04 -0500

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<HelloWorld boolean-property />
+<HelloWorld />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+@Options({
+    components: {
+        HelloWorld
+    }
+})
+export default class App extends Vue {
+
+}
+</script>
+
+<style>
+/* noinspection CssUnusedSymbol */
+#app {
+    background-position: top left;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2C3E50;
+}
+
+.main-nav {
+    padding: 30px;
+    text-align: center;
+}
+
+.main-nav a {
+    font-weight: bold;
+    color: #2C3E50;
+}
+
+.main-nav a.router-link-exact-active {
+    color: #42B983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+<p>Value is: {{ booleanProperty }},
+    Type is: {{ typeof booleanProperty }}</p>
+</template>
+
+<script lang="ts">
+import { Options, prop, Vue } from "vue-class-component";
+
+class Props {
+    public booleanProperty = prop<boolean>({});
+}
+
+export default class HelloWorld extends Vue.with(Props) {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42B983;
+}
+</style>
